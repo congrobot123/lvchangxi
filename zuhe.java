bac50bcbe099f458087e17d8f4dd65863167a7fa
lvchangxi <1607159782@qq.com>
2016-11-13 16:02:18 +0800

    20161113

---
+++ zuhe.java
@@ -0,0 +1,12 @@
+public class  zuhe
+{
+	public static void main(String[] args) 
+	{
+		String Head=head;
+		String Body=body;
+		String Arm=arm;
+		String Leg=leg;
+		String Person=person;
+		System.out.println("person="+head+body+arm+leg);
+	}
+}
