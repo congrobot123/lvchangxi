86f92cd87dcbb4d03a2c4af945ea88e918d2b5b6
lvchangxi <1607159782@qq.com>
2016-11-13 16:12:20 +0800

    20161113

---
+++ str.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class str 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scan=new Scanner (System.in);
+		System.out.println("输入第一个字符串：");
+		String a=scan.nextLine();
+		System.out.println("输入第二个字符串：");
+		String b=scan.nextLine();
+		
+		System.out.println(a+b);
+			
+	}
+}
